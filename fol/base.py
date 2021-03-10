b4b779059b7726e3acf80aebe97f3579ec89e31d
yinh <50171093+yinh-wx@users.noreply.github.com>
2021-03-10 17:00:28 +0800

    debug grammar_class

--- fol/base.py
+++ fol/base.py
@@ -1,8 +1,10 @@
 from abc import ABC
 
+
 class Formula(ABC):
     def __init__(self):
         pass
+
 
 class Variable(Formula):
     def __init__(self):
@@ -10,11 +12,13 @@ class Variable(Formula):
         print("claim a variable")
         pass
 
+
 class Conjunction(Formula):
     def __init__(self, lf, rf):
         super().__init__()
         print("claim a conjunction")
         self.lf, self.rf = lf, rf
+
 
 class Disjunction(Formula):
     def __init__(self, lf, rf):
@@ -22,11 +26,13 @@ class Disjunction(Formula):
         print("claim a disjunction")
         self.lf, self.rf = lf, rf
 
+
 class Negation(Formula):
     def __init__(self, f):
         super().__init__()
         print("claim a negation")
         self.f = f
+
 
 class Projection(Formula):
     def __init__(self, f):
@@ -51,21 +57,23 @@ def get_grammar_class(gc):
     biop = gc['biop']
     return delim, zop, uop, biop
 
+
 beta_query = {
-    '1p':   'p(e)',
-    '2p':   'p(p(e))',
-    '3p':   'p(p(p(e)))',
-    '2i':   'p(e)&p(e)',
-    '3i':   'p(e)&p(e)&p(e)',
-    '2in':  'p(e)&!p(e)',
-    '3in':  'p(e)&p(e)&!p(e)',
-    'inp':  'p(p(e)&!p(e))',
-    'pni':  'p(p(e))&!p(e)',
-    'ip':   'p(p(e)&p(e))',
-    'pi':   'p(e)&p(p(e))',
-    '2u':   'p(e)|p(e)',
-    'up':   'p(p(e)|p(e))'
+    '1p': 'p(e)',
+    '2p': 'p(p(e))',
+    '3p': 'p(p(p(e)))',
+    '2i': 'p(e)&p(e)',
+    '3i': 'p(e)&p(e)&p(e)',
+    '2in': 'p(e)&!p(e)',
+    '3in': 'p(e)&p(e)&!p(e)',
+    'inp': 'p(p(e)&!p(e))',
+    'pni': 'p(p(e))&!p(e)',
+    'ip': 'p(p(e)&p(e))',
+    'pi': 'p(e)&p(p(e))',
+    '2u': 'p(e)|p(e)',
+    'up': 'p(p(e)|p(e))'
 }
+
 
 def parse_beta_like(query, gc):
     delim, zop, uop, biop = get_grammar_class(gc)
@@ -86,13 +94,15 @@ def parse_beta_like(query, gc):
             else:
                 if pstack:
                     begin = pstack.pop(-1)
-                    if pstack: continue
-                    else: last_dilim_query = query[begin+1: i]
+                    if pstack:
+                        continue
+                    else:
+                        last_dilim_query = query[begin + 1: i]
                 else:
                     raise SyntaxError(f"Query {query} is Iiligal")
                 # address the only bracket case
                 if begin == 0 and i == len(query) - 1:
-                    return parse_beta_like(last_dilim_query)
+                    return parse_beta_like(last_dilim_query, grammar_class)
 
         elif c in biop:  # handle the conjunction and disjunction
             if len(pstack) == 0:  # only when at the top of the syntax tree
@@ -103,20 +113,18 @@ def parse_beta_like(query, gc):
                 uop_triggers.append([i, c])
 
     for i, c in biop_triggers:
-        lf, rf = parse_beta_like(query[:i]), parse_beta_like(query[i+1:])
+        lf, rf = parse_beta_like(query[:i], grammar_class), parse_beta_like(query[i + 1:], grammar_class)
         return biop[c](lf, rf)
     for i, c in uop_triggers:
-        f = parse_beta_like(query[i+1:])
+        f = parse_beta_like(query[i + 1:], grammar_class)
         return uop[c](f)
 
     raise SyntaxError(f"Query {query} fall out of branches")
-
-
 
 
 if __name__ == '__main__':
     for name in beta_query:
         case = beta_query[name]
         print(f'parsing the query {name}: `{case}`')
-        f = parse_beta_like(case)
+        f = parse_beta_like(case, grammar_class)
         print()
