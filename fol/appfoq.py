174efc4d91eea9340a40e2f4ee41a5e29c07e3a5
zihao <wangzihao820@hotmail.com>
2021-04-05 16:56:19 +0800

    update critiron in the AppFOQEstimator

--- fol/appfoq.py
+++ fol/appfoq.py
@@ -18,17 +18,20 @@ class AppFOQEstimator(ABC):
         pass
 
     @abstractclassmethod
-    def get_conjunction_embedding(self, lemb, remb):
+    def get_conjunction_embedding(self, lemb: torch.Tensor, remb: torch.Tensor):
         pass
 
     @abstractclassmethod
-    def get_disjunction_embedding(self, lemb, remb):
+    def get_disjunction_embedding(self, lemb: torch.Tensor, remb: torch.Tensor):
         pass
 
     @abstractclassmethod
-    def get_difference_embedding(self, lemb, remb):
+    def get_difference_embedding(self, lemb: torch.Tensor, remb: torch.Tensor):
         pass
 
+    @abstractclassmethod
+    def criterion(self, answer_emb: torch.Tensor, answer_estimation: List[IntList]):
+        pass
 
 class TransE_Tnorm(AppFOQEstimator, nn.Module):
     def __init__(self) -> None:
@@ -42,17 +45,20 @@ class TransE_Tnorm(AppFOQEstimator, nn.Module):
         xe = torch.tensor(entity_ids)
         return self.entity_embeddings(xe)
 
-    def get_projection_embedding(self, proj_ids: IntList, emb: torch.Tensor):
+    def get_projection_embedding(self, proj_ids: IntList, emb):
         assert emb.shape[0] == len(proj_ids)
         xp = torch.tensor(proj_ids)
         return self.relation_embeddings(xp) + emb
 
-    def get_conjunction_embedding(self, lemb: torch.Tensor, remb: torch.Tensor):
+    def get_conjunction_embedding(self, lemb, remb):
         assert lemb.shape
         return torch.minimum(lemb, remb)
 
-    def get_disjunction_embedding(self, lemb: torch.Tensor, remb: torch.Tensor):
+    def get_disjunction_embedding(self, lemb, remb):
         return torch.maximum(lemb, remb)
 
-    def get_difference_embedding(self, lemb: torch.Tensor, remb: torch.Tensor):
+    def get_difference_embedding(self, lemb, remb):
         return torch.clamp(lemb - remb, 0)
+
+    def criterion(self, answer_emb: torch.Tensor, answer_estimation: List[IntList]):
+        return super().criterion(answer_emb, answer_estimation)
