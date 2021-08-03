70e34919dd64d0de322fb77c20b2d13f280d0177
Zihao WANG <wangzihao820@hotmail.com>
2021-08-03 16:23:20 +0800

    runnable validation

--- fol/appfoq.py
+++ fol/appfoq.py
@@ -231,12 +231,17 @@ class BetaEstimator(AppFOQEstimator):
                                               embedding_dim=self.entity_dim * 2)
         self.relation_embeddings = nn.Embedding(num_embeddings=n_relation,
                                                 embedding_dim=self.relation_dim)
-        embedding_range = torch.tensor([(self.gamma + self.epsilon) / entity_dim]).to(self.device)
-        nn.init.uniform_(tensor=self.entity_embeddings.weight, a=-embedding_range.item(), b=embedding_range.item())
-        nn.init.uniform_(tensor=self.relation_embeddings.weight, a=-embedding_range.item(), b=embedding_range.item())
+        self.embedding_range = nn.Parameter(
+            torch.tensor([(self.gamma.item() + self.epsilon) / entity_dim]),
+            requires_grad=False
+            ).to(self.device)
+        nn.init.uniform_(tensor=self.entity_embeddings.weight, a=-self.embedding_range.item(), b=self.embedding_range.item())
+        nn.init.uniform_(tensor=self.relation_embeddings.weight, a=-self.embedding_range.item(), b=self.embedding_range.item())
+
         self.entity_regularizer = Regularizer(1, 0.05, 1e9)
         self.projection_regularizer = Regularizer(1, 0.05, 1e9)
-        self.intersection_net = BetaIntersection(self.entity_dim)
+        # self.intersection_net = BetaIntersection(self.entity_dim)
+        self.center_net = BetaIntersection(self.entity_dim)
         self.projection_net = BetaProjection(self.entity_dim * 2,
                                              self.relation_dim,
                                              hidden_dim,
@@ -250,6 +255,138 @@ class BetaEstimator(AppFOQEstimator):
     def get_projection_embedding(self, proj_ids: torch.LongTensor, emb):
         assert emb.shape[0] == len(proj_ids)
         rel_emb = self.relation_embeddings(proj_ids)
+        pro_emb = self.projection_net(emb, rel_emb)
+        return pro_emb
+
+    def get_conjunction_embedding(self, conj_emb: List[torch.Tensor]):
+        sub_alpha_list, sub_beta_list = [], []
+        for sub_emb in conj_emb:
+            sub_alpha, sub_beta = torch.chunk(sub_emb, 2, dim=-1)
+            sub_alpha_list.append(sub_alpha)  # b*dim
+            sub_beta_list.append(sub_beta)
+        all_alpha = torch.stack(sub_alpha_list)  # conj*b*dim
+        all_beta = torch.stack(sub_beta_list)
+        new_alpha, new_beta = self.intersection_net(all_alpha, all_beta)
+        embedding = torch.cat([new_alpha, new_beta], dim=-1)
+        return embedding
+
+    def get_disjunction_embedding(self, disj_emb: List[torch.Tensor]):
+        union_emb = torch.stack(disj_emb, dim=1)  # datch*disj*d
+        return union_emb
+
+    def get_negation_embedding(self, emb: torch.Tensor):
+        return 1. / emb
+
+    def get_difference_embedding(self, lemb: torch.Tensor, remb: torch.Tensor):  # a-b = a and(-b)
+        r_neg_emb = self.get_negation_embedding(remb)
+        return self.get_conjunction_embedding([lemb, r_neg_emb])
+
+    def criterion(self, pred_emb: torch.Tensor, answer_set: List[IntList], union: bool = False):
+        assert pred_emb.shape[0] == len(answer_set)
+        alpha_embedding, beta_embedding = torch.chunk(pred_emb, 2, dim=-1)
+        query_dist = torch.distributions.beta.Beta(alpha_embedding, beta_embedding)
+        chosen_ans, chosen_false_ans, subsampling_weight = \
+            inclusion_sampling(answer_set, negative_size=self.negative_size, entity_num=self.n_entity)  # todo: negative
+        answer_embedding = self.get_entity_embedding(
+            torch.tensor(chosen_ans, device=self.device)).squeeze()
+        if union:
+            positive_union_logit = self.compute_logit(answer_embedding.unsqueeze(1), query_dist)  # b*disj
+            positive_logit = torch.max(positive_union_logit, dim=1)[0]
+        else:
+            positive_logit = self.compute_logit(answer_embedding, query_dist)
+        all_neg_emb = self.get_entity_embedding(torch.tensor(chosen_false_ans, device=self.device).view(-1))
+        all_neg_emb = all_neg_emb.view(-1, self.negative_size, 2 * self.entity_dim)  # batch*negative*dim
+        if union:
+            query_dist_unsqueezed = torch.distributions.beta.Beta(alpha_embedding.unsqueeze(2),
+                                                                  beta_embedding.unsqueeze(2))
+            negative_union_logit = self.compute_logit(all_neg_emb.unsqueeze(1), query_dist_unsqueezed)
+            negative_logit = torch.max(negative_union_logit, dim=1)
+        else:
+            query_dist_unsqueezed = torch.distributions.beta.Beta(alpha_embedding.unsqueeze(1),
+                                                                  beta_embedding.unsqueeze(1))
+            negative_logit = self.compute_logit(all_neg_emb, query_dist_unsqueezed)  # b*negative
+        return positive_logit, negative_logit, subsampling_weight.to(self.device)
+
+    def compute_logit(self, entity_emb, query_dist):
+        entity_alpha, entity_beta = torch.chunk(entity_emb, 2, dim=-1)
+        entity_dist = torch.distributions.beta.Beta(entity_alpha, entity_beta)
+        logit = self.gamma - torch.norm(torch.distributions.kl.kl_divergence(entity_dist, query_dist), p=1, dim=-1)
+        return logit
+
+    def compute_all_entity_logit(self, pred_emb: torch.Tensor, union: bool = False) -> torch.Tensor:
+        all_entities = torch.LongTensor(range(self.n_entity)).to(self.device)
+        all_embedding = self.get_entity_embedding(all_entities)  # nentity*dim
+        pred_alpha, pred_beta = torch.chunk(pred_emb, 2, dim=-1)  # batch*(disj)*dim
+        query_dist = torch.distributions.beta.Beta(pred_alpha.unsqueeze(-2), pred_beta.unsqueeze(-2))
+        batch_num = find_optimal_batch(all_embedding, query_dist=query_dist,
+                                       compute_logit=self.compute_logit, union=union)
+        chunk_of_answer = torch.chunk(all_embedding, batch_num, dim=0)
+        logit_list = []
+        for answer_part in chunk_of_answer:
+            if union:
+                union_part = self.compute_logit(answer_part.unsqueeze(0).unsqueeze(0),
+                                                query_dist)  # b*disj*answer_part*dim
+                logit_part = torch.max(union_part, dim=1)[0]
+            else:
+                logit_part = self.compute_logit(answer_part.unsqueeze(dim=0), query_dist)  # batch*answer_part*dim
+            logit_list.append(logit_part)
+        all_logit = torch.cat(logit_list, dim=1)
+        return all_logit
+        
+
+class BetaEstimator4V(AppFOQEstimator):
+    """Beta embedding for verification
+    """
+    name = "beta"
+
+    def __init__(self, n_entity, n_relation, hidden_dim,
+                 gamma, entity_dim, relation_dim, num_layers,
+                 negative_sample_size, device):
+        super().__init__()
+        self.name = 'beta'
+        self.device = device
+        self.n_entity = n_entity
+        self.n_relation = n_relation
+        self.hidden_dim = hidden_dim
+        self.gamma = nn.Parameter(
+            torch.Tensor([gamma]),
+            requires_grad=False
+        )
+        self.epsilon = 2.0
+        self.negative_size = negative_sample_size
+        self.entity_dim, self.relation_dim = entity_dim, relation_dim
+        # self.entity_embeddings = nn.Embedding(num_embeddings=n_entity,
+                                            #   embedding_dim=self.entity_dim * 2)
+        # self.relation_embeddings = nn.Embedding(num_embeddings=n_relation,
+                                                # embedding_dim=self.relation_dim)
+        self.entity_embedding = nn.Parameter(torch.zeros(n_entity, self.entity_dim * 2))
+        self.relation_embedding = nn.Parameter(torch.zeros(n_relation, self.relation_dim))
+        self.embedding_range = nn.Parameter(
+            torch.tensor([(self.gamma.item() + self.epsilon) / entity_dim]),
+            requires_grad=False
+            ).to(self.device)
+        # nn.init.uniform_(tensor=self.entity_embeddings.weight, a=-self.embedding_range.item(), b=self.embedding_range.item())
+        # nn.init.uniform_(tensor=self.relation_embeddings.weight, a=-self.embedding_range.item(), b=self.embedding_range.item())
+        nn.init.uniform_(tensor=self.entity_embedding, a=-self.embedding_range.item(), b=self.embedding_range.item())
+        nn.init.uniform_(tensor=self.relation_embedding, a=-self.embedding_range.item(), b=self.embedding_range.item())
+
+        self.entity_regularizer = Regularizer(1, 0.05, 1e9)
+        self.projection_regularizer = Regularizer(1, 0.05, 1e9)
+        # self.intersection_net = BetaIntersection(self.entity_dim)
+        self.center_net = BetaIntersection(self.entity_dim)
+        self.projection_net = BetaProjection(self.entity_dim * 2,
+                                             self.relation_dim,
+                                             hidden_dim,
+                                             self.projection_regularizer,
+                                             num_layers)
+
+    def get_entity_embedding(self, entity_ids: torch.LongTensor):
+        emb = self.entity_embedding[entity_ids, :]
+        return self.entity_regularizer(emb)
+
+    def get_projection_embedding(self, proj_ids: torch.LongTensor, emb):
+        assert emb.shape[0] == len(proj_ids)
+        rel_emb = self.relation_embedding[proj_ids, :]
         pro_emb = self.projection_net(emb, rel_emb)
         return pro_emb
 
--- validation.py
+++ validation.py
@@ -1,21 +1,36 @@
-from typing import Dict, Set
-import logging
-import argparse
-from torch.utils.data.dataloader import DataLoader
-import yaml
+"""
+This file validate the correctness of our framework by
+comparing the BetaE model during optimization procedure.
+"""
 
-from KGReasoning.models import KGReasoning
+
+import argparse
+import logging
+import pickle
+from os.path import join
+from typing import Dict, List, Set, Tuple
+import json
+
+import yaml
+from torch.utils.data.dataloader import DataLoader
+import torch
+from torch.distributions.beta import Beta
+from torch.distributions.kl import kl_divergence
+
+from fol.appfoq import BetaEstimator4V
+from fol.foq_v2 import parse_formula
+from fol import beta_query_v2
+from main import compute_final_loss
+from transform_beta_data import transform_json_query
 from KGReasoning.dataloader import (SingledirectionalOneShotIterator,
                                     TrainDataset)
-                                    
-from fol.appfoq import BetaEstimator
-
+from KGReasoning.models import KGReasoning
+from KGReasoning.util import flatten_query
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--save_intermediate_results",
                     action="store_true",
-                    type="bool",
                     default=False)
 parser.add_argument("--begin_checkpoint",
                     type=str,
@@ -24,15 +39,38 @@ parser.add_argument("--model_config_file",
                     type=str,
                     default="config/validation.yaml")
 parser.add_argument("--cuda", type=bool, default=False)
+parser.add_argument("--batch_size", type=int, default=1)
+parser.add_argument("--lr", type=float, default=1e-4)
+parser.add_argument("--dataset_folder",
+                    type=str,
+                    default='data/FB15k-237-betae')
 
 
-def ref_train_step(beta_train_queries: Dict[str, Set],
-                   beta_train_answers: Dict[str, Set],
-                   model: KGReasoning, 
+query_name_dict = {('e', ('r',)): '1p',
+                   ('e', ('r', 'r')): '2p',
+                   ('e', ('r', 'r', 'r')): '3p',
+                   (('e', ('r',)), ('e', ('r',))): '2i',
+                   (('e', ('r',)), ('e', ('r',)), ('e', ('r',))): '3i',
+                   ((('e', ('r',)), ('e', ('r',))), ('r',)): 'ip',
+                   (('e', ('r', 'r')), ('e', ('r',))): 'pi',
+                   (('e', ('r',)), ('e', ('r', 'n'))): '2in',
+                   (('e', ('r',)), ('e', ('r',)), ('e', ('r', 'n'))): '3in',
+                   ((('e', ('r',)), ('e', ('r', 'n'))), ('r',)): 'inp',
+                   (('e', ('r', 'r')), ('e', ('r', 'n'))): 'pin',
+                   (('e', ('r', 'r', 'n')), ('e', ('r',))): 'pni',
+                   (('e', ('r',)), ('e', ('r',)), ('u',)): '2u-DNF',
+                   ((('e', ('r',)), ('e', ('r',)), ('u',)), ('r',)): 'up-DNF',
+                   ((('e', ('r', 'n')), ('e', ('r', 'n'))), ('n',)): '2u-DM',
+                   ((('e', ('r', 'n')), ('e', ('r', 'n'))), ('n', 'r')): 'up-DM'
+                   }
+
+
+def ref_train_step(beta_train_queries: List[Tuple[str, Tuple]],
+                   train_answers: Dict[Tuple, Set],
+                   model: KGReasoning,
                    optimizer,
                    nentity,
                    nrelation,
-                   batch_size,
                    negative_sample_size) -> Dict:
     """
     Conduct a train step from KG reasoning
@@ -42,19 +80,81 @@ def ref_train_step(beta_train_queries: Dict[str, Set],
                      nentity,
                      nrelation,
                      negative_sample_size,
-                     beta_train_answers),
-        batch_size=batch_size,
+                     train_answers),
+        batch_size=len(beta_train_queries),
         shuffle=False,
         num_workers=1,
         collate_fn=TrainDataset.collate_fn
     ))
-    log = model.train_step(model, optimizer, train_iterator, args)
+    log = model.train_step(model, optimizer, train_iterator, args, step=None)
     return log
 
 
-def our_train_step(batch_data, formula, model: BetaEstimator, optimizer) -> Dict:
-    pass
+def our_train_step(batch_train_queries,
+                   positive_sample,
+                   negative_sample,
+                   subsampling_weight,
+                   model: BetaEstimator4V,
+                   optimizer) -> Dict:
+    optimizer.zero_grad()
+    positive_logits, negative_logits = [], []
+    for query, query_structure in batch_train_queries:
+        query_name = query_name_dict[query_structure]
+        formula = beta_query_v2[query_name]
+        query_instance = parse_formula(formula)
+        query_instance.additive_ground(
+            json.loads(
+                transform_json_query(query, query_name)))
+        pred_embedding = query_instance.embedding_estimation(model)
+        pred_alpha, pred_beta = torch.chunk(pred_embedding, 2, dim=-1)
+        pred_dist = Beta(pred_alpha, pred_beta)
+        pred_dist_unsqueezed = Beta(pred_alpha.unsqueeze(1),
+                                    pred_beta.unsqueeze(1))
 
+        positive_embedding = model.get_entity_embedding(
+            positive_sample.view(-1)).view(-1, 2 * model.entity_dim)
+        positive_logit = model.compute_logit(positive_embedding, pred_dist)
+        positive_logits.append(positive_logit)
+
+        negative_embedding = model.get_entity_embedding(
+            negative_sample.view(-1)).view(
+                -1, model.negative_size, 2 * model.entity_dim)
+        negative_logit = model.compute_logit(negative_embedding, 
+                                             pred_dist_unsqueezed)
+        negative_logits.append(negative_logit)
+    positive_logits = torch.cat(positive_logits) 
+    negative_logits = torch.cat(negative_logits)
+    pos_loss, neg_loss = compute_final_loss(positive_logits, negative_logits, 
+                                            subsampling_weight)
+    loss = pos_loss + neg_loss
+    loss /= 2
+    loss.backward()
+    optimizer.step()
+    log = {
+        'positive_sample_loss': pos_loss.item(),
+        'negative_sample_loss': neg_loss.item(),
+        'loss': loss.item(),
+    }
+    return log
+    
+def check_model_sync(ref_model, our_model):
+    ref_state_dict = ref_model.state_dict()
+    our_state_dict = our_model.state_dict()
+    tensor_diff_keys = []
+    grad_diff_keys = []
+    for k in ref_state_dict:
+        tensor_ref = ref_state_dict[k]
+        grad_ref = tensor_ref.grad
+        tensor_our = our_state_dict[k]
+        grad_our = tensor_our.grad
+        if not (tensor_our == tensor_ref).all():
+            tensor_diff_keys.append(k)
+        
+        if grad_our is not None and not (grad_our == grad_ref).all():
+            grad_diff_keys.append(k)
+    return tensor_diff_keys, grad_diff_keys    
+        
+    
 
 # def ref_eval_step(batch_data, model) -> Dict:
     # pass
@@ -66,6 +166,73 @@ def our_train_step(batch_data, formula, model: BetaEstimator, optimizer) -> Dict
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    logging.basicConfig(filename="logs/kgr_validation.log",
+                        filemode="wt",
+                        level=logging.INFO)
     with open(args.model_config_file, 'rt') as f:
         model_config = yaml.load(f, Loader=yaml.FullLoader)
     ref_model_config = model_config['ref']
+    our_model_config = model_config['our']
+    ref_model = KGReasoning(query_name_dict=query_name_dict,
+                            **ref_model_config)
+    our_model = BetaEstimator4V(**our_model_config)
+    # model syncronization
+    our_model.load_state_dict(ref_model.state_dict())
+    check_model_sync(ref_model, our_model)
+    ref_opt = torch.optim.Adam(ref_model.parameters(), lr=args.lr)
+    our_opt = torch.optim.Adam(our_model.parameters(), lr=args.lr)
+    with open(f"{args.dataset_folder}/stats.txt", 'rt') as f:
+        entrel = f.readlines()
+        nentity = int(entrel[0].split(' ')[-1])
+        nrelation = int(entrel[1].split(' ')[-1])
+
+    train_queries = pickle.load(
+        open(join(args.dataset_folder, "train-queries.pkl"), 'rb'))
+    train_answers = pickle.load(
+        open(join(args.dataset_folder, "train-answers.pkl"), 'rb'))
+    # valid_queries = pickle.load(open(join(args.dataset_folder, "valid-queries.pkl"), 'rb'))
+    # valid_hard_answers = pickle.load(open(join(args.dataset_folder, "valid-hard-answers.pkl"), 'rb'))
+    # valid_easy_answers = pickle.load(open(join(args.dataset_folder, "valid-easy-answers.pkl"), 'rb'))
+    # test_queries = pickle.load(open(join(args.dataset_folder, "test-queries.pkl"), 'rb'))
+    # test_hard_answers = pickle.load(open(join(args.dataset_folder, "test-hard-answers.pkl"), 'rb'))
+    # test_easy_answers = pickle.load(open(join(args.dataset_folder, "test-easy-answers.pkl"), 'rb'))
+    flat_train_queries = flatten_query(train_queries)
+    train_size = len(flat_train_queries)
+
+
+    for i in range(0, train_size, args.batch_size):
+        batch_train_queries = flat_train_queries[i: i+args.batch_size]
+        ref_log = ref_train_step(batch_train_queries,
+                                 train_answers,
+                                 ref_model,
+                                 ref_opt,
+                                 nentity,
+                                 nrelation,
+                                 negative_sample_size=128)
+
+        positive_sample = ref_log['positive_sample']
+        negative_sample = ref_log['negative_sample']
+        subsampling_weight = ref_log['subsampling_weight']
+        
+        our_log = our_train_step(batch_train_queries,
+                                 positive_sample,
+                                 negative_sample,
+                                 subsampling_weight,
+                                 our_model,
+                                 our_opt)
+        
+        for k in our_log:
+            assert our_log[k] == ref_log[k]
+            logging.info(f"step {i}, {k} = {our_log[k]} is validated for ref and our model")
+        
+        tdk, gdk = check_model_sync(ref_model, our_model)
+        
+        if len(tdk) != 0 or len(gdk) != 0:
+            for k in tdk:
+                logging.error(f"step {i} tensor value {k} differs")
+            for k in gdk:
+                logging.error(f"step {i} gradient value {k} differs")
+        else:
+            logging.info(f"step {i}, tensors and gradients of two models are the same")
+            
+        
