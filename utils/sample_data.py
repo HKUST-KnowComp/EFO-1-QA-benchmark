cc94ac21647c519198d20658ee4510fcbc211a4a
yinh <yinh-wx@outlook.com>
2021-08-19 15:55:00 +0800

    ans_statistic

--- utils/sample_data.py
+++ utils/sample_data.py
@@ -11,6 +11,7 @@ def sampling_stored(query_list, store_fold, num_queries, projs, projs_hard, rpro
         query_structure = beta_query_v2[query_name]
         query_instance = parse_formula(query_structure)
         save_data = collections.defaultdict(list)
+        all_query = set()
         i = int(0)
         while i < num_queries:
             if backward:
@@ -18,18 +19,18 @@ def sampling_stored(query_list, store_fold, num_queries, projs, projs_hard, rpro
             else:
                 full_ans = query_instance.random_query(projs_hard)
             assert query_instance.deterministic_query(projs_hard) == full_ans
-            if len(full_ans) > 100:
+            easy_ans = query_instance.deterministic_query(projs)
+            hard_ans = full_ans - easy_ans
+            if len(hard_ans) > 100 or len(hard_ans) == 0 or query_instance.dumps in all_query:
                 query_instance.lift()
                 continue
             if mode == 'test' or mode == 'valid':
-                easy_ans = query_instance.deterministic_query(projs)
-                hard_ans = full_ans - easy_ans
-                if len(hard_ans) != 0:
-                    save_data['query'].append(query_instance.dumps)
-                    save_data['id'].append(i)
-                    i += 1
-                    save_data['easy_answer_set'].append(easy_ans)
-                    save_data['hard_answer_set'].append(hard_ans)
+                save_data['query'].append(query_instance.dumps)
+                all_query.add(query_instance.dumps)
+                save_data['id'].append(i)
+                i += 1
+                save_data['easy_answer_set'].append(easy_ans)
+                save_data['hard_answer_set'].append(hard_ans)
             elif mode == 'train':
                 if len(full_ans) > 0:
                     save_data['query'].append(query_instance.dumps)
@@ -45,19 +46,22 @@ def sampling_stored(query_list, store_fold, num_queries, projs, projs_hard, rpro
 
 
 if __name__ == "__main__":
-    data_path = 'data/test_benchmark/FB15k-237-foq'
+    data_path = 'data/test_benchmark/FB15k-237-valid-foq-22803'
     read_data_path = 'data/FB15k-237-betae'
     ent2id, rel2id, projection_train, reverse_projection_train, projection_valid, reverse_projection_valid, \
         projection_test, reverse_projection_test = load_data_with_indexing(read_data_path)
     NewLook_Query = ['2D', '3D', 'Dp']
     p_Query = ['1p', '2p', '3p']
+
     '''
     sampling_stored(p_Query, data_path, 10798, None,
                     projection_train, reverse_projection_train, 'train', True)
     sampling_stored(p_Query, data_path, 4000, projection_train, projection_valid,
                     reverse_projection_valid, 'valid', True)
-                    '''
-    sampling_stored(p_Query, data_path, 10000, projection_train, projection_test,
+                   '''
+    sampling_stored(['1p'], data_path, 22804, projection_valid, projection_test,
+                    reverse_projection_test, 'test', True)
+    sampling_stored(['2p', '3p'], data_path, 5000, projection_valid, projection_test,
                     reverse_projection_test, 'test', True)
 
 
