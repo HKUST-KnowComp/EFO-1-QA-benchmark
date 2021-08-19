1f08a42ccaa82cc5bb0fe369cfca022e16463ac8
Zihao WANG <wangzihao820@hotmail.com>
2021-08-19 16:41:00 +0800

    fix

--- benchmark_sampling.py
+++ benchmark_sampling.py
@@ -1,8 +1,9 @@
 from collections import defaultdict
 import os.path as osp
 import os
-import logging
 from multiprocessing import Pool
+import argparse
+from numpy.core.records import _deprecate_shape_0_as_None
 
 from tqdm import tqdm
 import pandas as pd
@@ -42,11 +43,11 @@ def sample_by_row(row, easy_proj, easy_rproj, hard_proj):
     return list(easy_answers), list(hard_answers), results
 
 
-def sample_by_row_final(row, easy_proj, easy_rproj, hard_proj):
+def sample_by_row_final(row, easy_proj, hard_proj, hard_rproj,):
     while True:
         query_instance = parse_formula(row.original)
-        easy_answers = query_instance.backward_sample(easy_proj, easy_rproj)
-        full_answers = query_instance.deterministic_query(hard_proj)
+        full_answers = query_instance.backward_sample(hard_proj, hard_rproj)
+        easy_answers = query_instance.deterministic_query(easy_proj)
         hard_answers = full_answers.difference(easy_answers)
         results = normal_forms_transformation(query_instance)
         if 0 < len(hard_answers) <= 100:
@@ -54,51 +55,60 @@ def sample_by_row_final(row, easy_proj, easy_rproj, hard_proj):
     return list(easy_answers), list(hard_answers), results
 
 
-if __name__ == "__main__":
-    df = pd.read_csv("logs/generated_formula_anchor_node=3.csv")
-    beta_data_folders = ["data/FB15k-237-betae",
-                         "data/FB15k-betae",
-                         "data/NELL-betae"]
-    for data_path in beta_data_folders:
-        ent2id, rel2id, \
-            proj_train, reverse_train, \
-            proj_valid, reverse_valid, \
-            proj_test, reverse_test = load_data_with_indexing(data_path)
+def get_query_dataframe(row, easy_proj, hard_proj, hard_rproj, cpus=12, sample_size=5000):
+    def sampler_func(i):
+        row_data = {}
+        easy_answers, hard_answers, results = sample_by_row_final(
+            row, easy_proj, hard_proj, hard_rproj)
+        row_data['easy_answers'] = easy_answers
+        row_data['hard_answers'] = hard_answers
+        for k in results:
+            row_data[k] = results[k].dumps
+        return row_data
 
-        kg_name = osp.basename(data_path).replace("-betae", "")
-        out_folder = osp.join("data", "benchmark", kg_name)
-        os.makedirs(out_folder, exist_ok=True)
-
-        for i, row in tqdm(df.iterrows(), total=len(df)):
-            fid = row.formula_id
-            data = defaultdict(list)
-            def sampler_func(i):
-                row_data = {}
-                row_data['query_id'] = f"{fid}-sample{i:04d}"
-                easy_answers, hard_answers, results = sample_by_row_final(
-                    row, proj_train, reverse_train, proj_test)
-                row_data['easy_answers'] = easy_answers
-                row_data['hard_answers'] = hard_answers
-                for k in results:
-                    row_data[k] = results[k].dumps
-                return row_data
-     
-            with Pool(12) as p:
-                gets = p.map(sampler_func, list(range(10000)))
-                for row_data in gets:
+    fid = row.formula_id
+    data = defaultdict(list)
+    generated_original_dumps = set()
+    produced_size = 0
+    while produced_size < sample_size:
+        with Pool(cpus) as p:
+            gets = p.map(sampler_func,
+                         list(range(sample_size - produced_size)))
+            for row_data in gets:
+                if row_data['original'] in generated_original_dumps:
+                    continue
+                else:
+                    generated_original_dumps.add(
+                        row_data['original']
+                    )
+                    produced_size += 1
                     for k in row_data:
                         data[k].append(row_data[k])
 
-            pd.DataFrame(data).to_csv(osp.join(out_folder, f"data-{fid}.csv"), index=False)
-#           for i in tqdm(range(10000), leave=False, desc=row.original + fid):
-#               query_id = f"{fid}-sample{i:04d}"
-#               data['query_id'].append(query_id)
-#               easy_answers, hard_answers, results = sample_by_row_final(
-#                   row, proj_train, reverse_train, proj_test)
-#               data['easy_answers'].append(easy_answers)
-#               data['hard_answers'].append(hard_answers)
-#               for k in results:
-#                   data[k].append(results[k].dumps)
+    pd.DataFrame(data).iloc[sample_size, :].to_csv(
+        osp.join(out_folder, f"data-{fid}.csv"), index=False)
 
-                
-        
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--graph", type=str)
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("logs/generated_formula_anchor_node=3.csv")
+    beta_data_folders = {"fb15k-237": "data/FB15k-237-betae",
+                         "fb15k": "data/FB15k-betae",
+                         "nell": "data/NELL-betae"}
+
+    args = parser.parse_args()
+    data_path = beta_data_folders[args.graph]
+    ent2id, rel2id, \
+        proj_train, reverse_train, \
+        proj_valid, reverse_valid, \
+        proj_test, reverse_test = load_data_with_indexing(data_path)
+
+    kg_name = osp.basename(data_path).replace("-betae", "")
+    out_folder = osp.join("data", "benchmark", kg_name)
+    os.makedirs(out_folder, exist_ok=True)
+
+    for i, row in tqdm(df.iterrows(), total=len(df)):
+        get_query_dataframe(row, proj_valid, proj_test, reverse_test)
