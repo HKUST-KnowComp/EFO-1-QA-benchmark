cbba64c1921bce8d0a3d35ed95a88331d988a78c
yinh <yinh-wx@outlook.com>
2021-08-29 16:38:31 +0800

    delete useless

--- utils/transform_beta_data.py
+++ utils/transform_beta_data.py
@@ -4,7 +4,7 @@ import pickle
 from fol.sampler import *
 from fol.foq import parse_foq_formula
 from fol.foq_v2 import parse_formula
-from fol.test_foq_v2 import beta_query_v2
+from fol import beta_query_v2
 from utils.util import load_rawdata_with_indexing
 
 query_name_dict = {('e', ('r',)): '1p',
